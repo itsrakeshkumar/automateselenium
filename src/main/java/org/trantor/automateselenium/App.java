c582e330d563d36508e2fde76c8e3a4f7e79f38c
Happy Geek <itshappygeek@gmail.com>
2024-04-11 13:07:21 +0530

    Creating a new project for testing html page elements

---
+++ src/main/java/org/trantor/automateselenium/App.java
@@ -0,0 +1,13 @@
+package org.trantor.automateselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
